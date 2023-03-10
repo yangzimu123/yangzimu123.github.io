12bd0254be20e6e611b1c829f7b7abbb36049bb4
yangzimu123 <121797399+yangzimu123@users.noreply.github.com>
2023-03-10 18:57:39 +0800

    Add files via upload

--- xc-malio-firsysyep.sh
+++ xc-malio-firsysyep.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # FileName: run.sh
 echo“检查python版本”
-
+Python3 --version
 echo;
 cd /tmp
 echo "输入开机密码“
-sudo mkdir mali
+sudo mkdir malio
 cd malio
 sudo curl -o python-3.11.2.pkg https://www.python.org/ftp/python/3.11.2/python-3.11.2-macos11.pkg
 open python-3.11.2.pkg
